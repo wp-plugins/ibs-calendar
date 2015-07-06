fa783986875e15fbd9f1a670a4c17a1bfb542f39
hmoore71 <hmoore71@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-06 18:43:34 +0000

    Update to Fullcalendar 2.3.2
    git-svn-id: https://plugins.svn.wordpress.org/ibs-calendar/tags/2.0@1193579 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/widget-ibs-calendar.php
@@ -0,0 +1,198 @@
+<?php
+
+class IBS_WCalendar extends WP_Widget {
+
+    public function __construct() {
+        $widget_ops = array(
+            'class' => 'ibs_wcalendar',
+            'description' => 'A widget to display a full calendar'
+        );
+
+        parent::__construct(
+                'ibs_wcalendar', 'IBS Calendar', $widget_ops
+        );
+    }
+
+    public function form($instance) {
+        $widget_defaults = array(
+            'title' => 'IBS Calendar',
+            'list_max' => 50,
+            'list_repeat' => "no",
+            'list_past' => "no",
+            'ibs_events' => "no",
+            'cal_height' => 300,
+            'has_list' => 'none',
+            'lst_height' => 300,
+        );
+
+        $instance = wp_parse_args((array) $instance, $widget_defaults);
+        $args = get_option('ibs_calendar_options');
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php echo'Title'; ?></label>
+            <input type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" class="widefat" value="<?php echo esc_attr($instance['title']); ?>">
+        </p>
+        <p></p>
+        <div class="widefat"><label for="<?php echo $this->get_field_id('has_list'); ?>"><span  style="display:inline-block; width:100px;"><?php echo 'List events'; ?></span>
+                <select id="<?php echo $this->get_field_id('has_list'); ?>" name="<?php echo $this->get_field_name('has_list'); ?>" >
+                    <option value="none" <?php if ($instance['has_list'] === 'none') echo 'selected'; ?> >No list</option>
+                    <option value="show" <?php if ($instance['has_list'] === 'show') echo 'selected'; ?> >Show list</option>
+                    <option value="hide" <?php if ($instance['has_list'] === 'hide') echo 'selected'; ?> >Hide list</option>
+                </select>
+            </label></div>
+        <p></p>
+
+        <div class="widefat"><label for="<?php echo $this->get_field_id('list_max'); ?>"><span  style="display:inline-block; width:100px;"><?php echo 'List max events'; ?></span>
+                <input type="number" min=1 max=100 id="<?php echo $this->get_field_id('list_max'); ?>" name="<?php echo $this->get_field_name('list_max'); ?>"  value="<?php echo esc_attr($instance['list_max']); ?>">
+            </label></div>
+        <p></p>
+
+        <div class="widefat"><label for="<?php echo $this->get_field_id('list_past'); ?>"><span  style="display:inline-block; width:100px;"><?php echo 'List past events'; ?></span>
+                <input type="checkbox" id="<?php echo $this->get_field_id('list_past'); ?>" name="<?php echo $this->get_field_name('list_past'); ?>"  <?php echo esc_attr($instance['list_past']) === 'yes' ? 'checked' : ''; ?> value="yes">
+            </label></div>
+        <p></p>
+
+        <div class="widefat"><label for="<?php echo $this->get_field_id('list_repeat'); ?>"><span  style="display:inline-block; width:100px;"><?php echo 'List repeat events'; ?></span>
+                <input type="checkbox" id="<?php echo $this->get_field_id('list_repeat'); ?>" name="<?php echo $this->get_field_name('list_repeat'); ?>"  <?php echo esc_attr($instance['list_repeat']) === 'yes' ? 'checked' : ''; ?>  value="yes">
+            </label></div>
+        <p></p>
+        <div class="widefat"><label for="<?php echo $this->get_field_id('ibs_events'); ?>"><span  style="display:inline-block; width:100px;"><?php echo 'Show IBS Events'; ?></span>
+                <input type="checkbox" id="<?php echo $this->get_field_id('ibs_events'); ?>" name="<?php echo $this->get_field_name('ibs_events'); ?>"  <?php echo esc_attr($instance['ibs_events']) === 'yes' ? 'checked' : ''; ?>  value="yes">
+            </label></div>
+        <p></p>
+        <div class="widefat"><label for="<?php echo $this->get_field_id('cal_height'); ?>"><span  style="display:inline-block; width:100px;"><?php echo 'Calendar height'; ?></span>
+                <input type="number" min=200 max=1000 id="<?php echo $this->get_field_id('cal_height'); ?>" name="<?php echo $this->get_field_name('cal_height'); ?>"  value="<?php echo esc_attr($instance['cal_height']); ?>">
+                &nbsp;px</label></div>
+        <p></p>
+        <div class="widefat"><label for="<?php echo $this->get_field_id('lst_height'); ?>"><span  style="display:inline-block; width:100px;"><?php echo 'List height'; ?></span>
+                <input type="number"  min=200  max=1000 id="<?php echo $this->get_field_id('lst_height'); ?>" name="<?php echo $this->get_field_name('lst_height'); ?>" value="<?php echo esc_attr($instance['lst_height']); ?>">
+                &nbsp;px</label></div>
+        <p></p>
+        <div class="widefat"><label><?php echo '<strong>Feeds to show</strong>'; ?></label></div>
+        <p></p>
+        <?php
+        for ($feed = 1; $feed <= $args['feedCount']; $feed++) {
+            $curr_feed = "feed_" . $feed;
+            if (isset($args['feeds'][$curr_feed]['name']) && $args['feeds'][$curr_feed]['name'] !== '') {
+                $name = $args['feeds'][$curr_feed]['name'];
+            } else {
+                $name = $curr_feed;
+            }
+            $fn = $this->get_field_name($curr_feed);
+            $ln = $this->get_field_id($curr_feed);
+            $checked = isset($instance[$curr_feed]);
+            $checked = $checked ? 'checked' : '';
+            echo "<div class='widefat' style='margin-left:100px;' ><label for='$ln'><input name='$fn' value='$feed' type='checkbox' $checked /><span> $name</span></label></div>";
+        }
+        ?>
+        <p></p>
+        <?PHP
+    }
+
+    public function update($new_instance, $old_instance) {
+        $old_instance = $new_instance;
+
+        $instance['title'] = $new_instance['title'];
+
+        return $old_instance;
+    }
+
+    static function fixBool(&$item, $key) {
+        if ($key === 'eventLimit') {
+            return;
+        }
+        switch (strtolower($item)) {
+            case "null" : $item = null;
+                break;
+            case "true" :
+            case "yes" : $item = true;
+                break;
+            case "false" :
+            case "no" : $item = false;
+                break;
+            default :
+        }
+    }
+
+    public function widget($widget_args, $instance) {
+        extract($widget_args);
+        $title = apply_filters('widget_title', $instance['title']);
+        echo $before_widget;
+        if ($title) {
+            echo $before_title . $title . $after_title;
+        }
+        $feeds = array();
+        foreach ($instance as $key => $value) {
+
+            if (false !== strpos($key, 'feed')) {
+                $feeds[] = $value;
+            }
+        }
+        $args = get_option('ibs_calendar_options');
+        array_walk_recursive($args, array(__CLASS__, 'fixBool'));
+        for ($i = 1; $args['feedCount'] >= $i; $i++) {
+            $index = (string) $i;
+            if (false == in_array($index, $feeds)) {
+                $args['feeds']['feed_' . $i]['enabled'] = false;
+            }
+        }
+        $args['list_max'] = (int) $instance['list_max'];
+        $args['list_past'] = isset($instance['list_past']);
+        $args['list_repeat'] = isset($instance['list_repeat']);
+        $args['ibsEvents'] = isset($instance['ibs_events']);
+        $args['width'] = '100%';
+        $args['weekends'] = true;
+        $args['theme'] = false;
+        $args['editable'] = false;
+        $args['event_list'] = $instance['has_list'];
+        $args['defaultView'] = 'month';
+        $args['headerLeft'] = 'prev';
+        $args['headerCenter'] = 'title';
+        $args['headerRight'] = 'next';
+        $args['titleFormat'] = 'MMM YYYY';
+        $args['dayNamesShort'] = array('Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa');
+        $args['eventLimit'] = false;
+        $args['eventLimitClick'] = 'popover';
+        $args['aspectRatio'] = null;
+        $args['height'] = (int) $instance['cal_height'];
+        $args['ajaxData'] = array("action" => "ibs_calendar_ajax", "type" => "event");
+        $args['ajaxUrl'] = admin_url("admin-ajax.php");
+        $args['id'] = $widget_id;
+        $id = $widget_id;
+        $width = $args['width'];
+        $html = '<div id="ibs-calendar-id" class="aligncenter" style="width:650px;" >
+            <form id="fullcalendar-id" >
+                <div id="ibs-loading-id" ></div>
+            </form>
+            <div>
+                <input id="list-display-id" type="checkbox" style="margin : 10px;" />
+                <span> &nbsp;Event List</span>
+                <div id="event-list-id" style="max-height:200px; overflow:auto; display:none" >
+                    <table id="event-table-id" style="width:100%;">
+                        <tbody> 
+                        </tbody>
+                    </table>
+                </div>       
+            </div>
+        </div>';
+        $html = str_replace('-id', '-' . $id, $html);
+        $html = str_replace('width:650px;', 'width :' . $width . ';', $html);
+        $html = str_replace('max-height:200px;', 'max-height:' . $instance['lst_height'] . 'px;', $html);
+        echo $html;
+        ?>
+        <script type="text/javascript">
+            jQuery(document).ready(function ($) {
+                new CalendarObj(jQuery, <?PHP echo json_encode($args); ?>, 'widget');
+            });
+        </script> 
+        <?php
+        echo $after_widget;
+    }
+
+}
+
+function ibs_register_widget() {
+    register_widget('IBS_WCalendar');
+}
+
+add_action('widgets_init', 'ibs_register_widget');
---
+++ readme.txt
@@ -0,0 +1,69 @@
+﻿=== IBS Calendar ===
+Contributors: hmoore71
+Donate link: https://indianbendsolutions.net/donate/
+Plugin URI: https://indianbendsolutions.net/documentation/ibs-Calendar/
+Author URI: https://indianbendsolutions.net/
+Tags: google calendar, calendar, 
+Requires at least: 4.0
+Tested up to: 4.2.2
+Stable tag: 2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Implementation of Adam Shaw's Full Calendar for public Google Calendar feeds.
+
+== Description ==
+* IBS Calendar is a comprehensive implementation of the jQuery Full Calendar Version 2 from Adam Shaw. See http://fullcalendar.io/. 
+* Define any practical number of Google Calendar feeds each having a different color scheme.
+* Style tooltips using Qtip2 predefined styles.
+* Shortcode generator to help customize individual shortcodes. 
+* IBS Calendar Widget showing calendar and optionally an event list.
+* Displays IBS Events plugin post-type ibs_event.
+* See more at https://indianbendsolutions.net/documentation/ibs-Calendar/
+
+
+Presently IBS Calendar is in its Beta phase of development and all testing and reporting of issues is appreciated.
+
+== Installation ==
+1. Download ibs-Calendar and unzip.
+2. Upload `ibs-Calendar` folder to the Wordpress plugin directory
+3. Activate the plugin through the ‘Plugins’ menu in WordPress
+4 Admin | Settings menu | IBS Calendar and configure the plugin.
+
+== Frequently Asked Questions ==
+How do I get the Google Calendar feed address? 
+1. Open your Google Calendar and on the left side bar click "My calendars" which should list all of your calendars.
+2. To the right of your calendar name is a dropdown indicator; click it and a dialog will display.
+3. Click "Calendar settings" and that will open a page with all of your calendar settings on it.
+4. Towards the bottom are a set of three buttons XML(orange) ICAL(green) HTML(blue) and a Calendar ID. 
+5. Click the XML(orange) and a dialog will open displaying the link to the calendar. Copy and paste this link into the IBS Calendar feed address field.
+6. Alternatively you may also copy and paste the Calendar ID into the IBS Calendar Feed.
+
+What is "Google API Key" ? Google requires every user of the Google Calendar feeds to have their own Google Calendar API Key. IBS Calendar has its own key that is shared with all users of the plugin.
+If the use of the key gets too high (500,000 requests per day) the plugin may be denied access. At that point you may want to obtain another key.
+
+Can I display event sources other than Google Calendar? Perhaps. If the event information conforms to Full Calendar expectations. As of Version 0.4 if IBS Events plugin is installed those events may be displayed.
+
+How can I hide the event titles? Because calendars can be configured such that the event titles do not display properly an option, hideTitle, has be added. When this option is set the event will be displayed on the Full Calendar using only the background color.
+This yields a solid bar as the event title.
+
+== Screenshots ==
+1. Settings tab.
+2. Options for Full Calendar.
+3. Feeds for Google Calendars
+5. Shortcode generator.
+6. Widget
+7. Widget admin
+
+== Changelog ==
+
+(initial release)
+2014-11-27 Added required Google Calendar API Key
+2014-12-02 Version 0.3 adds a IBS calendar widget using Full Calendar.
+2014-12-27 Version 0.4 adds several more Full Calendar options and support for IBS Events calendar feed.
+2014-13-28 Version 0.5 fixes IBS Event display problem.
+2015-01-06 Version 1.0 Adds new features and expands existing functionality 
+2015-01-07 Version 1.1 Syncs to base plugin. Modified Admin setup.
+2015-07-03 Version 1.2 Fixes Google API Key
+
+== Upgrade Notice ==
